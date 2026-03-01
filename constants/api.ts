f52e0b65dec06f316f2aed0a832bf9eba7809627
aseoane1 <seoanedeoisantonio@gmail.com>
2026-03-01 08:03:43 +0100

    feat: apk configs

--- constants/api.ts
+++ constants/api.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://10.37.61.210:8000";
+
+const BASE_URL = "http://10.20.30.25:8000";
 
 export async function fetchAPI(path: string, options?: RequestInit) {
     const res = await fetch(BASE_URL + path, {
